6ac22d3dfd32695f60ebcecccad4e11853470b88
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-02-05 12:00:57 +0800

    added create user page

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     
 ]
