dd3165b82e6de9c3eeba40d0317192b09afc93f5
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-02-08 19:43:35 +0800

    showed user story by primary key

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,9 +3,24 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from news.models import NewsStory
+
+
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
 
+
+class UserProfile(generic.DetailView):
+    model =CustomUser
+    template_name = 'users/userProfile.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_story'] = NewsStory.objects.filter(author=self.kwargs['pk'])
+
+        print(context['user_story'])
+        return context
